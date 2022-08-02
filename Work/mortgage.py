e146b4ef71152a782d682a386cbacb94d52f1c9f
abannie <uckies@gmail.com>
2022-08-02 16:36:53 -0700

    mortgage.py - extra k for 1yr

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -7,9 +7,17 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+payment_num = 0
+extra_payment = 1000
 
 while principal > 0:
-    principal = principal * (1+rate/12) - payment
-    total_paid = total_paid + payment
+    if payment_num < 12:
+	new_payment = payment + extra_payment
+    	principal = principal * (1+rate/12) - new_payment
+    	total_paid = total_paid + new_payment
+	payment_num += 1
+    else:
+    	principal = principal * (1+rate/12) - payment
+    	total_paid = total_paid + payment
 
 print('Total paid', total_paid)
