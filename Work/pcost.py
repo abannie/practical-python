506e1058a8e75546cafdc8b8abef1eb9797d11cf
abannie <uckies@gmail.com>
2022-09-01 20:23:55 -0700

    pcost function

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,16 +2,22 @@
 #
 # Exercise 1.27
 fname = "/home/annie/projects/practical-python/Work/Data/portfolio.csv"
-with open(fname, 'rt') as f:
-	headers = next(f).split(',')
-	print(headers)
-	total = 0.0
-	for line in f:
-		row = line.split(',')
-		subtotal = int(row[1])*float(row[2].strip())
-		print(f'{subtotal}')
-		#fields = row.split(',')
-		#print(fields)
-		total = total + subtotal
-	print(f'Total cost {total}')
+def portfolio_cost(fname):
+	with open(fname, 'rt') as f:
+		headers = next(f).split(',')
+		print(headers)
+		total = 0.0
+		for line in f:
+			row = line.split(',')
+			try:
+				subtotal = int(row[1])*float(row[2].strip())
+				print(f'{subtotal}')
+				total = total + subtotal
+			except ValueError:
+				print("couldn't parse", row)
 
+		return total
+		#print(f'Total cost {total}')
+
+cost = portfolio_cost(fname)
+print('Total cost:', cost)
