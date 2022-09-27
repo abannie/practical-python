8034416c96b49ad54ba652d44da456baab82a5fb
abannie <uckies@gmail.com>
2022-09-26 21:12:18 -0700

    calculate networth

--- Work/report.py
+++ Work/report.py
@@ -1,9 +1,10 @@
 # report.py
 #
-# Exercise 2.4
+# Exercise 2.4 (list of tuples), 2.5 (list of dict), 2.6 (dict), 2.7(apply for use case)
 
 import csv
 
+#Ex 2.4, 2.6
 def read_portfolio(filename):
     '''Computes the total cost (shares*price) of a portfolio file'''
     portfolio = []
@@ -16,16 +17,26 @@ def read_portfolio(filename):
             portfolio.append(holding)
     return portfolio
 
+#Ex 2.6
 def read_prices(filename):
     '''read (current) prices of stock'''
 
-    portfolio = {}
+    stocks = {}
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         for row in rows:
             print(row)
             if row:
-                portfolio[row[0]] = float(row[1])
+                stocks[row[0]] = float(row[1])
             else:
                 print("read prices row error", row)
-    return portfolio
+    return stocks
+
+def compute_networth(portfolio, stock_prices):
+
+    networth = 0
+    for stock in portfolio:
+        stock_val = (stock_prices[ stock['name'] ] * stock['shares']) - (stock['shares'] * stock['price'])
+        print(f"{stock['name']} today value: {stock_val}")
+        networth += stock_val
+    print(f'networth today: {networth}')
